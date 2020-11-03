b1a2dd1fa890a26f8d5ed92d68139eea2991be5c
jessye <jessye95@gmail.com>
2020-11-04 00:33:06 +0100

    update usequeryform hook test

--- src/tests/useQueryForm.test.tsx
+++ src/tests/useQueryForm.test.tsx
@@ -1,16 +1,48 @@
-import {cleanup} from '@testing-library/react'
-import {renderHook} from '@testing-library/react-hooks'
-import 'fake-indexeddb/auto'
+import {renderHook, act} from '@testing-library/react-hooks'
 import useQueryForm from '../hooks/useQueryForm'
+import {selectInput, circleAttr} from './utils'
+import 'fake-indexeddb/auto'
+import {cleanup} from '@testing-library/react'
 
 afterEach(cleanup)
 
 describe('test hook', () => {
-  const {result, waitForNextUpdate} = renderHook(() => useQueryForm())
+  const {result, waitForNextUpdate, waitForValueToChange} = renderHook(() =>
+    useQueryForm(),
+  )
 
-  it('validates form values', async () => {
-    result.current.handleDrawShape()
+  it('validates query form', async () => {
+    await act(() => {
+      result.current.handleDrawShape()
+    })
     await waitForNextUpdate()
     expect(result.current.error).toContain('Invalid')
+  })
+
+  it('updates shape attributes', async () => {
+    const newColour = '#ffff00'
+    act(() => {
+      result.current.handleEditShape(circleAttr)
+    })
+
+    await waitForValueToChange(() => result.current.activeId)
+
+    expect(result.current.activeId).toBe(circleAttr.id)
+    expect(result.current.currentColour).toBe(circleAttr.colour)
+    expect(result.current.currentShapeName).toBe(circleAttr.type)
+    expect(result.current.radius).toBe(circleAttr.radius)
+
+    act(() => {
+      result.current.handleInputChange(selectInput(newColour, 'colour'))
+    })
+
+    await waitForNextUpdate()
+    expect(result.current.currentColour).toBe(newColour)
+
+    await act(async () => {
+      result.current.handleDrawShape()
+    })
+    await waitForValueToChange(() => result.current.shapes)
+    expect(result.current.shapes?.[0].colour).toBe(newColour)
   })
 })
