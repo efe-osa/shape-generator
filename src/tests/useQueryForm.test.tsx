d2fc6f68c3bbd93837588f941e42d92e97afe8cc
jessye <jessye95@gmail.com>
2020-11-04 00:15:03 +0100

    testing usequeryform hook

---
+++ src/tests/useQueryForm.test.tsx
@@ -0,0 +1,19 @@
+import {renderHook, act} from '@testing-library/react-hooks'
+import useQueryForm from '../hooks/useQueryForm'
+import {selectInput, circleAttr} from './utils'
+import 'fake-indexeddb/auto'
+import {cleanup} from '@testing-library/react'
+
+afterEach(cleanup)
+
+describe('test hook', () => {
+  const {result, waitForNextUpdate} = renderHook(() => useQueryForm())
+
+  it('validates query form', async () => {
+    act(() => {
+      result.current.handleDrawShape()
+    })
+    await waitForNextUpdate()
+    expect(result.current.error).toContain('Invalid')
+  })
+})
